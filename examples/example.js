47ac99aacd6e098252058c0b3c88390ff0984bec
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-09 23:10:33 -0400

    fix: passing in string argument instead of object

--- examples/example.js
+++ examples/example.js
@@ -1,6 +1,6 @@
 import Cachier from 'cachier-js';
 
-const client = new Cachier({ url: 'http://localhost:8000' });
+const client = new Cachier('http://localhost:8000');
 
 const setCache = async (key, value, expiry) => {
     const response = await client.set(key, value, expiry);
