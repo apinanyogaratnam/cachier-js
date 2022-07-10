62d3f023a4258a58cc112648276892b431bbe028
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-09 23:05:21 -0400

    style: fixed spacing

--- examples/example.js
+++ examples/example.js
@@ -3,13 +3,13 @@ import Cachier from 'cachier-js';
 const client = new Cachier({ url: 'http://localhost:8000' });
 
 const setCache = async (key, value) => {
-  const response = await client.set(key, value);
-  console.log(response);
+    const response = await client.set(key, value);
+    console.log(response);
 }
 
 const getCache = async (key) => {
-  const response = await client.get(key);
-  console.log(response);
+    const response = await client.get(key);
+    console.log(response);
 }
 
 setCache();
