5cbe06036a77b665a71d71483577b4cdccc38e14
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-09 22:19:42 -0400

    feat: implemented get and set methods for interacting with cachier service

--- Cachier.js
+++ Cachier.js
@@ -1,13 +1,37 @@
+import axios from 'axios';
+
 class Cachier {
     constructor(url) {
         this.url = url;
     }
 
-    get(key) {
+    async get(key) {
         if (!key) return null;
+
+        try {
+            const response = await axios.get(`${this.url}?cache_key=${key}`);
+            return response.data.value;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
     }
 
-    set(key, value, expiry) {
+    async set(key, value, expiry) {
         if (!key) return false;
+
+        const data = {
+            cache_key: key,
+            cache_value: value,
+            cache_expiry: expiry,
+        };
+
+        try {
+            const response = axios.post(this.url, data);
+            return response.data.is_saved_successfully;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
     }
 }
